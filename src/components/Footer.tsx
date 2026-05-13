a46dfb042776a15106e6bc3197bd095124fd6a72
cloudsasha <35777990+cloudsasha@users.noreply.github.com>
2026-05-13 15:07:20 -0700

    Update Footer.tsx
    Switched copyright to automatically show what year it is with >© {new Date().getFullYear()} Universe Unlocked. All rights reserved.</p>

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,7 +7,7 @@ interface FooterProps {
 export default function Footer({ isDarkMode }: FooterProps) {
   return (
     <footer className={`flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t ${isDarkMode ? 'border-gray-700' : 'border-gray-200'}`}>
-      <p className={`text-xs ${isDarkMode ? 'text-gray-400' : 'text-gray-500'}`}>© 2025 Universe Unlocked. All rights reserved.</p>
+      <p className={`text-xs ${isDarkMode ? 'text-gray-400' : 'text-gray-500'}`}>© {new Date().getFullYear()} Universe Unlocked. All rights reserved.</p>
       <nav className="sm:ml-auto flex gap-4 sm:gap-6">
        
       </nav>
